4d0021f6922910cce0d1122b20ff6522f43d59f8
branagram <abrishg73@gmail.com>
2020-12-02 16:54:20 +0300

    let itbe

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -137,5 +137,8 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "abrishg73@gmail.com"
 EMAIL_HOST_PASSWORD = 'yoratorad@abraham3'
+
+django_heroku.settings(locals())
+
 
 
